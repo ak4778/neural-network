b94c74d1f9fadf37960d95a25e5b4a540166eaf1
ak4778 <30285931+ak4778@users.noreply.github.com>
2017-10-09 16:06:52 +0800

    Update neuralredis.c

--- neuralredis.c
+++ neuralredis.c
@@ -2,6 +2,7 @@
  * as a Redis module.
  *
  * Check https://github.com/antirez/neural-redis/ for more information
+ * http://www.cnblogs.com/qw12/tag/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0/default.html?page=2
  *
  */
 
